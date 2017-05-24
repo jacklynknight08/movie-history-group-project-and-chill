1e22eaf9c9b5c5bcf094203a3d434a41653bd02a
Madeline Power <madelinepower@Madelines-MacBook-Pro.local>
2017-05-24 14:23:02 -0500

    complete login, logout and getUser functions

--- javascripts/user-login.js
+++ javascripts/user-login.js
@@ -1,17 +1,27 @@
 "use strict";
+let firebase = require("./firebaseConfig"),
+    provider = new firebase.auth.GoogleAuthProvider(),
+    currentUser = null;
 
 function logInGoogle() {
-	//return fb authrorization
+	return firebase.auth().signInWithPopup(provider);
 }
 
 function logoutGoogle() {
-	//return fb signoutAuth
+	return firebase.auth().signOut();
 }
 
 function getUser() {
-	//return currentUser
+	return currentUser;
 }
 
-//fbAuth
+firebase.auth().onAuthStateChanged(function(user){
+  if (user) {
+    currentUser = user.uid;
+  } else{
+    currentUser = null;
+    console.log("NO USER LOGGED IN");
+  }
+});
 
 module.exports = {logInGoogle, logoutGoogle, getUser};
