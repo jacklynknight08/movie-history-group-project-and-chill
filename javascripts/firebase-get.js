cdafeedad01b81df209adbd67bf470a7beb138d4
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-05-24 15:58:15 -0500

    removed api info for firebase

--- javascripts/firebase-get.js
+++ javascripts/firebase-get.js
@@ -1,11 +1,7 @@
 "use strict";
 
 function getFBKey() {
-	return {
-        apiKey: "AIzaSyB7-Xby7dg7JvmQkjebNHuExFx_zJlShWY",
-        authDomain: "and-chill-5eeba.firebaseapp.com",
-        databaseURL: "https://and-chill-5eeba.firebaseio.com"
-    };
+
 }
 
 
