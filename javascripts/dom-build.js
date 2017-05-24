a4d99da8530ed497470115a874243ad3c59fd13c
Madeline Power <madelinepower@Madelines-MacBook-Pro.local>
2017-05-24 15:42:28 -0500

    some stuff.. not sure what to do next

---
+++ javascripts/dom-build.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let $ = require('jquery');
+
+function makeMovieCards(movieList) {
+  //set up materialize columns and rows
+  //calls handlebars template
+  //gets result from template and appends everything to the DOM
+}
+
+module.exports = {makeMovieCards};
