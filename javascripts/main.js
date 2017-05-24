5b9c1405f67709f377c2184ec0be3d4ecc958395
Willie Pruitt <w_pruitt@hotmail.com>
2017-05-24 13:55:29 -0500

    Finished all but dom-build function shell

---
+++ javascripts/main.js
@@ -0,0 +1,29 @@
+"strict";
+
+function loadAPIMoviesToDOM() {
+	//just takes API searched data and loads to dom
+}
+
+function loadFBMoviesToDOM() {
+
+}
+
+function buildMovieObj() {
+	//takes card created by converting API data to card and pushes to FB
+	//also pushs rated and favorite movies to FB by adding/changing a key
+}
+
+//.add_to_watch eventListener 
+
+//search-btn eventListener (calls findMovies() and getWatchlist())
+
+
+//untracked-btn eventListener (callsfindMovies() and getWatchlist() with null values)
+//unwatched/watched/favs class=.search-btn
+
+//login-btn eventListener (calls loginGoogle())
+
+//logout-btn eventListener (calls logoutGoogle())
+
+//stars eventListener (calls rateMovie())
+
