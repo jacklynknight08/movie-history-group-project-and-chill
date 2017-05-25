7957644f4c1a2d5102be9f4020fc1f25ae0cc580
Madeline Power <madelinepower@Madelines-MacBook-Pro.local>
2017-05-25 10:28:15 -0500

    this will NOT work without db interact

--- javascripts/dom-build.js
+++ javascripts/dom-build.js
@@ -8,4 +8,22 @@ function makeMovieCards(movieList) {
   //gets result from template and appends everything to the DOM
 }
 
+function filterAPIObj(data) {
+  let apiArray = data.results;
+  let newMovieArray = [];
+  console.log(apiArray);
+  for (var i = 0; i < apiArray.length; i++) {
+      let newMovieObj = new Object({});
+      newMovieObj.id = apiArray[i].id;
+      newMovieObj.poster_path = apiArray[i].poster_path;
+      newMovieObj.title = apiArray[i].title;
+      newMovieObj.overview = apiArray[i].overview;
+      newMovieObj.release_date = apiArray[i].release_date;
+      // newMovieObj.actors;
+  newMovieArray.push(newMovieObj);
+  }
+console.log("newMovieArray", newMovieArray);
+// $('#movie-card-div').html(cardTemplate(newMovieArray));
+}
+
 module.exports = {makeMovieCards};
