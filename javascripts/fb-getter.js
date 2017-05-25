c21e0b39108758b4c122c132036fdf68b895a27a
Jackie Knight <jnk08@outlook.com>
2017-05-25 11:15:14 -0500

    added fb-getter file

---
+++ javascripts/fb-getter.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function getFBKey(){
+	return {
+		apiKey: "AIzaSyB7-Xby7dg7JvmQkjebNHuExFx_zJlShWY",
+		authDomain: "http://and-chill-5eeba.firebaseapp.com/",
+		databaseURL: "https://and-chill-5eeba.firebaseio.com/"
+	};
+}
+
+module.exports = getFBKey;
